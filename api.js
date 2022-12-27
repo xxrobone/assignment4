dc69f386a0db812784ef2f21fd6f50fb2d678ac1
robonexx <robertwagar@gmail.com>
2022-12-27 12:38:32 +0100

    search function works Next up pagination then add differnt fetch buttons.

--- api.js
+++ api.js
@@ -1,5 +1,8 @@
 console.log('api js file connected');
 import { API_URL, API_KEY } from './api_keys/keys.js';
+
+const SEARCH_URL = `${API_URL}?key=${API_KEY}&search=`;
+
 let btn_get_games = document.querySelector('.get_btn');
 
 btn_get_games.addEventListener('click', () => {
@@ -18,8 +21,6 @@ async function fetchAPI() {
       console.error(err.message);
     });
 }
-
-
 
 async function createGameList(item) {
   let ul = document.querySelector('#game_list');
@@ -53,25 +54,27 @@ async function createGameList(item) {
     ul.innerHTML = html;
   });
 }
-/* 
-let search = '';
+
 const form = document.getElementById('form');
+
 form.addEventListener('submit', function (e) {
   e.preventDefault();
   // so many ways of acutally doing this :D
 
-  let newSearch = document.querySelector('#search').value;
-  console.log(
-    'this is the new search value inside submit function: ' + newSearch
-  );
-}); */
-/* 
-const handleSearch = () => {
+  let search = document.querySelector('#search').value;
+  console.log('this is the new search value inside submit function: ' + search);
+
+  handleSearch(search);
+});
+
+const handleSearch = async (search) => {
   fetch(SEARCH_URL + `${search}`)
     .then((res) => res.json())
-    .then((data) => console.log(data.results));
+    .then((data) => {
+      console.log(data.results);
+      createGameList(data.results);
+    });
 };
- */
 
 // could get form data like this, maybe better when getting more input data
 /* const data = new FormData(form);
@@ -79,95 +82,3 @@ const handleSearch = () => {
       console.log(name, ':', value);
     }
       */
-
-
-    
-/* 
-added
-: 
-18389
-added_by_status
-: 
-{yet: 469, owned: 10641, beaten: 5123, toplay: 545, dropped: 942, …}
-background_image
-: 
-"https://media.rawg.io/media/games/456/456dea5e1c7e3cd07060c14e96612001.jpg"
-clip
-: 
-null
-dominant_color
-: 
-"0f0f0f"
-esrb_rating
-: 
-{id: 4, name: 'Mature', slug: 'mature'}
-genres
-: 
-(2) [{…}, {…}]
-id
-: 
-3498
-metacritic
-: 
-91
-name
-: 
-"Grand Theft Auto V"
-parent_platforms
-: 
-(3) [{…}, {…}, {…}]
-platforms
-: 
-(7) [{…}, {…}, {…}, {…}, {…}, {…}, {…}]
-playtime
-: 
-72
-rating
-: 
-4.47
-rating_top
-: 
-5
-ratings
-: 
-(4) [{…}, {…}, {…}, {…}]
-ratings_count
-: 
-6054
-released
-: 
-"2013-09-17"
-reviews_count
-: 
-6131
-reviews_text_count
-: 
-43
-saturated_color
-: 
-"0f0f0f"
-short_screenshots
-: 
-(7) [{…}, {…}, {…}, {…}, {…}, {…}, {…}]
-slug
-: 
-"grand-theft-auto-v"
-stores
-: 
-(5) [{…}, {…}, {…}, {…}, {…}]
-suggestions_count
-: 
-413
-tags
-: 
-(19) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-tba
-: 
-false
-updated
-: 
-"2022-12-25T20:03:55"
-user_game
-: 
-null
-*/
