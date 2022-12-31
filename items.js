4035a92a6eb6d46297a73621563cc67694ac4d9d
robonexx <robertwagar@gmail.com>
2022-12-31 02:04:01 +0100

    made an easy version of a pagination, I want to try to do it a bit different, learning

---
+++ items.js
@@ -0,0 +1,199 @@
+const items = [
+  {
+    title: 'sometitle1',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle12',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle13',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle14',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle15',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle16',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle6',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle10',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle102',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle4',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle30',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle123',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle122',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle763',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle22',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle33',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle44',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle55',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle66',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle77',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle88',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle99',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle234',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle345',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle4567',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle567',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle678',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle1098',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle8933',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle345',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle23',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle34',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle45',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle55',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle45',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle1234',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle34',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle1',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+  {
+    title: 'sometitle23',
+    score: '8',
+    desc: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aut pariatur adipisci libero possimus quia tempora ex, odio corporis. Quo cumque enim ullam rem voluptate quos iusto quam iste officia aspernatur.',
+  },
+];
+
+export default items;
